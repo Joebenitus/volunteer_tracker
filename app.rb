c3397c63ccc4bd96336a85ba82190685bf262321
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-09 11:57:48 -0700

    add view files and add routing to allow the creation of projects

--- app.rb
+++ app.rb
@@ -1,78 +1,34 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/album')
-require('./lib/song')
+require('./lib/project')
+require('./lib/volunteer')
 require('pry')
 also_reload('lib/**/*.rb')
 require('pg')
 require 'dotenv/load'
 
-DB = PG.connect({:dbname => 'record_store', :password => ENV['PG_PASS']})
+DB = PG.connect({:dbname => 'volunteer_tracker_test', :password => ENV['PG_PASS']})
 
 get('/') do
-  redirect to('/albums')
+  redirect to('/projects')
 end
 
-get('/albums') do
-  @albums = Album.all
-  erb(:albums)
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
 end
 
-get ('/albums/new') do
-  erb(:new_album)
+get('/projects/new') do
+  erb(:new_project)
 end
 
-post ('/albums') do
-  name = params[:album_name]
-  album = Album.new({:name => name, :id => nil})
-  album.save()
-  redirect to('/albums')
+post('/projects') do
+  title = params[:project_title]
+  project = Project.new({ title: title, id: nil})
+  project.save
+  redirect to('/projects')
 end
 
-get ('/albums/:id') do
-  @album = Album.find(params[:id].to_i())
-  erb(:album)
-end
-#
-get ('/albums/:id/edit') do
-  @album = Album.find(params[:id].to_i())
-  erb(:edit_album)
-end
-
-patch ('/albums/:id') do
-  @album = Album.find(params[:id].to_i())
-  @album.update(params[:name])
-  redirect to('/albums')
-end
-
-delete ('/albums/:id') do
-  @album = Album.find(params[:id].to_i())
-  @album.delete()
-  redirect to('/albums')
-end
-
-get ('/albums/:id/songs/:song_id') do
-  @song = Song.find(params[:song_id].to_i())
-  erb(:song)
-end
-
-post ('/albums/:id/songs') do
-  @album = Album.find(params[:id].to_i())
-  song = Song.new({:name => params[:song_name], :album_id => @album.id, :id => nil})
-  song.save()
-  erb(:album)
-end
-
-patch ('/albums/:id/songs/:song_id') do
-  @album = Album.find(params[:id].to_i())
-  song = Song.find(params[:song_id].to_i())
-  song.update(params[:name], @album.id)
-  erb(:album)
-end
-
-delete ('/albums/:id/songs/:song_id') do
-  song = Song.find(params[:song_id].to_i())
-  song.delete
-  @album = Album.find(params[:id].to_i())
-  erb(:album)
+get('/projects/:id') do
+  erb(:project)
 end
