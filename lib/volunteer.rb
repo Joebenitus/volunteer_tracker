65084af951373e2ac7eb67b25bda4e68c5f29b0b
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-09 10:50:47 -0700

    add passing test for 'save' method for 'Volunteer' class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -17,10 +17,15 @@ class Volunteer
     volunteers = []
     returned_volunteers.each do |volunteer|
       name = volunteer.fetch('name')
-      project_id = volunteer.fetch('project_id')
-      id = volunteer.fetch('id')
+      project_id = volunteer.fetch('project_id').to_i
+      id = volunteer.fetch('id').to_i
       volunteers.push(Volunteer.new({ name: name, project_id: project_id, id: id }))
     end
     volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id")
+    @id = result.first.fetch('id').to_i
   end
 end
