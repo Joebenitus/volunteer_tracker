8898fc6e3484d850fd63bd8c0bf0a6d1ce11c0f9
Joseph Karnafel <josephkarnafel@gmail.com>
2020-10-09 10:11:12 -0700

    add passing test for accessing 'name' property for 'Volunteer' class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,2 +1,10 @@
 class Volunteer
+  attr_reader :id
+  attr_accessor :project_id, :name
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
 end
